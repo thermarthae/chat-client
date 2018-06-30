409d00c14f4d6435133dd3a0db1cfaad85c0e001
thermarthae <thermarthae@protonmail.com>
2018-06-30 16:12:32 +0200

    Display path error in console

--- src/components/error.component.tsx
+++ src/components/error.component.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage } from "react-intl";
 import "../style/error.component.scss";
 
 const Error = ({ location }: any) => {
+	console.error("Error 404: ", location.pathname);
 	return (
 		<div id="error">
 			<FormattedMessage id="error" />
