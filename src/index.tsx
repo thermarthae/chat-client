f1cef3586c474d991c42558a2303ce738b653df3
thermarthae <thermarthae@protonmail.com>
2018-07-04 18:40:20 +0200

    Added apollo-link-ws

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,18 @@ import { AppContainer } from "react-hot-loader";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from "apollo-link";
 import { onError } from "apollo-link-error";
 import { withClientState } from "apollo-link-state";
-import { ApolloLink } from "apollo-link";
 import { BatchHttpLink } from "apollo-link-batch-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getOperationAST } from "graphql/utilities/getOperationAST";
+
 import defaults from "./state/defaults";
 import mutations from "./state/mutations";
 // import queries from "./apollo/state/resolvers/queries";
 
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-
 import "./style/index.scss";
 import App from "./components/app.component";
 
@@ -44,10 +46,25 @@ const client = new ApolloClient({
 			if (networkError) console.warn("networkError (logout)", networkError); // logoutUser();
 		}),
 		stateLink,
-		new BatchHttpLink({
-			uri: "http://localhost:3000/graphql",
-			credentials: "include"
-		})
+		ApolloLink.split(
+			operation => {
+				const operationAST = getOperationAST(operation.query, operation.operationName);
+				return !!operationAST && operationAST.operation === "subscription";
+			},
+			new WebSocketLink({
+				uri: `ws://localhost:3000/graphql`,
+				options: {
+					reconnect: true,
+					connectionParams: {
+						authToken: "user.authToken",
+					}
+				}
+			}),
+			new BatchHttpLink({
+				uri: "http://localhost:3000/graphql",
+				credentials: "include"
+			}),
+		)
 	])
 });
 client.onResetStore(stateLink.writeDefaults as any);
