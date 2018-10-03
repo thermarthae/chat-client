8cfbd9906bb8121c8e4203e80b020eda591f3b36
thermarthae <thermarthae@protonmail.com>
2018-10-03 17:56:02 +0200

    change Menu SFC => PureComponent

--- src/components/Chat/Menu/Menu.tsx
+++ src/components/Chat/Menu/Menu.tsx
@@ -9,26 +9,26 @@ import './Menu.style.scss';
 import Header from './Header';
 import NavFilters from './NavFilters/NavFilters';
 
-const Menu = () => {
-	return <div id='menu'>
-		<Header />
-		<div className='wrapper'>
-			<NavFilters />
-			<div className='separator' />
-			<List className='container'>
-				<ListItem className='line' button>
-					<FormattedMessage id='chat.menu.help'>
-						{txt => <span className='name'>{txt}</span>}
-					</FormattedMessage>
-				</ListItem>
-				<ListItem className='line' button>
-					<FormattedMessage id='chat.menu.settings'>
-						{txt => <span className='name'>{txt}</span>}
-					</FormattedMessage>
-				</ListItem>
-			</List>
-		</div>
-	</div>;
-};
-
-export default Menu;
+export default class Menu extends React.PureComponent {
+	public render() {
+		return <div id='menu'>
+			<Header />
+			<div className='wrapper'>
+				<NavFilters />
+				<div className='separator' />
+				<List className='container'>
+					<ListItem className='line' button>
+						<FormattedMessage id='chat.menu.help'>
+							{txt => <span className='name'>{txt}</span>}
+						</FormattedMessage>
+					</ListItem>
+					<ListItem className='line' button>
+						<FormattedMessage id='chat.menu.settings'>
+							{txt => <span className='name'>{txt}</span>}
+						</FormattedMessage>
+					</ListItem>
+				</List>
+			</div>
+		</div>;
+	}
+}
