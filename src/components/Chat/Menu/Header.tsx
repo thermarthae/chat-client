81c1387700a57b26832e96f4ab2c0494f5dd44c7
thermarthae <thermarthae@protonmail.com>
2018-10-05 17:33:43 +0200

    change Header SFC => PureComponent

--- src/components/Chat/Menu/Header.tsx
+++ src/components/Chat/Menu/Header.tsx
@@ -4,17 +4,17 @@ import { FormattedMessage } from 'react-intl';
 import IconButton from '@material-ui/core/IconButton';
 import PersonAdd from '@material-ui/icons/PersonAdd';
 
-const Header = () => {
-	return (
-		<div className='head header'>
-			<FormattedMessage id='chat.menu.title'>
-				{txt => <span className='title'>{txt}</span>}
-			</FormattedMessage>
-			<IconButton className='btn'>
-				<PersonAdd style={{ fontSize: 'inherit' }} />
-			</IconButton>
-		</div>
-	);
-};
-
-export default Header;
+export default class Header extends React.PureComponent {
+	public render() {
+		return (
+			<div className='head header'>
+				<FormattedMessage id='chat.menu.title'>
+					{txt => <span className='title'>{txt}</span>}
+				</FormattedMessage>
+				<IconButton className='btn'>
+					<PersonAdd style={{ fontSize: 'inherit' }} />
+				</IconButton>
+			</div>
+		);
+	}
+}
