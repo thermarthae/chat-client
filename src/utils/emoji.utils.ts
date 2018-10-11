1a08d6953a38d0ac0aee6cebb6a76dd9eac4d139
thermarthae <thermarthae@protonmail.com>
2018-10-11 19:55:27 +0200

    remove regex lookbehinds due compatibility problems

--- src/utils/emoji.utils.ts
+++ src/utils/emoji.utils.ts
@@ -1,12 +1,13 @@
 import twemoji from 'twemoji';
 
-export const getImgRegex = /<img\s+[^>]*class="emoji"[^>]*>/g;
-export const getAltRegex = /(?<=alt=")(.*?)(?=")/g;
+const getImgRegex = /<img\s+[^>]*class="emoji"[^>]*>/g;
 
 export const parseEmoji = (text: string) => {
 	const withTwemoji = twemoji.parse(text, { folder: 'svg', ext: '.svg' });
 	return withTwemoji.replace(getImgRegex, img => {
-		const emoji = String(img.match(getAltRegex)) || '';
-		return `<span class="emoji-wrapper">${img}<span>${emoji}</span></span>`;
+		const alt = img.match(/alt="(.*?)"/g);
+		if (!alt) return '';
+		const emoji = alt[0].split('"')[1];
+		return !emoji ? '' : `<span class="emoji-wrapper">${img}<span>${emoji}</span></span>`;
 	});
 };
