d2407625f5a15a094cc80e7b1ea98c14a4bf0f2c
thermarthae <thermarthae@protonmail.com>
2018-12-20 19:45:40 +0100

    remove PersonAdd from Menu Header

--- src/components/Chat/Menu/Header.tsx
+++ src/components/Chat/Menu/Header.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import IconButton from '@material-ui/core/IconButton';
-import PersonAdd from '@material-ui/icons/PersonAdd';
-
 export default class Header extends React.PureComponent {
 	public render() {
 		return (
@@ -11,9 +8,6 @@ export default class Header extends React.PureComponent {
 				<FormattedMessage id='chat.menu.title'>
 					{txt => <span className='title'>{txt}</span>}
 				</FormattedMessage>
-				<IconButton className='btn'>
-					<PersonAdd style={{ fontSize: 'inherit' }} />
-				</IconButton>
 			</div>
 		);
 	}
