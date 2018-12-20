1faaf3ac2cda23186c43f87796313a08b266a81f
thermarthae <thermarthae@protonmail.com>
2018-12-20 19:56:51 +0100

    change pure to dumb in Menu Header component

--- src/components/Chat/Menu/Header.tsx
+++ src/components/Chat/Menu/Header.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-export default class Header extends React.PureComponent {
-	public render() {
-		return (
-			<div className='head header'>
-				<FormattedMessage id='chat.menu.title'>
-					{txt => <span className='title'>{txt}</span>}
-				</FormattedMessage>
-			</div>
-		);
-	}
-}
+const Header = () => (
+	<div className='head header'>
+		<FormattedMessage id='chat.menu.title'>
+			{txt => <span className='title'>{txt}</span>}
+		</FormattedMessage>
+	</div>
+);
+export default Header;
