1f376da751cf8bd5d4a17a554dfed7b86debcf3c
thermarthae <thermarthae@protonmail.com>
2018-12-20 20:34:30 +0100

    use JSS in MenuItem

--- src/components/Chat/Menu/MenuItem/MenuItem.tsx
+++ src/components/Chat/Menu/MenuItem/MenuItem.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 import ListItem from '@material-ui/core/ListItem';
+import menuItemStyles from './MenuItem.style';
 
 interface IMenuItemProps {
 	l18nID: string;
@@ -15,21 +16,20 @@ const MenuItem = React.memo(({ setInboxFilter, active, l18nID, count }: IMenuIte
 	let countElement = null;
 	switch (typeof count) {
 		case 'number':
-			countElement = <span className='count'>{count}</span>;
+			countElement = <span>{count}</span>;
 			break;
 		case 'object': //if null
 			countElement = <CircularProgress size='1em' color='inherit' />;
 			break;
 	}
+	const classes = menuItemStyles({});
 
 	return (
 		<ListItem button
 			onClick={setInboxFilter}
-			className={'line' + (active ? ' active' : '')}
+			className={classes.root + (active ? (' ' + classes.active) : '')}
 		>
-			<FormattedMessage id={l18nID}>
-				{txt => <span className='name'>{txt}</span>}
-			</FormattedMessage>
+			<FormattedMessage id={l18nID} />
 			{countElement}
 		</ListItem>
 	);
