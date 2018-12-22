c57b61f46df9955d47a68bb8f54a2fc7d5ae27b2
thermarthae <thermarthae@protonmail.com>
2018-12-22 17:04:32 +0100

    use JSS in ScrollDownInfo

---
+++ src/components/Chat/Mailbox/Inbox/ScrollDownInfo/ScrollDownInfo.style.ts
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/styles';
+import { Theme } from '@material-ui/core';
+
+const scrollDownInfoStyles = makeStyles(({ palette, typography }: Theme) => {
+	const em07 = typography.emToPx(0.7);
+	return {
+		root: {
+			position: 'absolute',
+			margin: 'auto',
+			width: 'max-content',
+			maxWidth: '50%',
+			right: 0,
+			left: 0,
+			bottom: 0,
+			padding: em07,
+			borderRadius: `${em07} ${em07} 0px 0px`,
+			color: palette.textLight.primary,
+			cursor: 'pointer',
+			textAlign: 'center',
+			backgroundColor: palette.primary.main,
+			boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
+		},
+	};
+}, { name: 'ScrollDownInfo' });
+export default scrollDownInfoStyles;
--- src/components/Chat/Mailbox/Inbox/ScrollDownInfo/ScrollDownInfo.tsx
+++ src/components/Chat/Mailbox/Inbox/ScrollDownInfo/ScrollDownInfo.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Slide from '@material-ui/core/Slide';
+import scrollDownInfoStyles from './ScrollDownInfo.style';
 
 interface IScrollDownInfo {
 	open: boolean;
 	onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 const ScrollDownInfo = ({ open, onClick }: IScrollDownInfo) => {
+	const classes = scrollDownInfoStyles({});
 	return (
 		<Slide direction='up' in={open} mountOnEnter unmountOnExit>
-			<div className='scrollDownInfo' onClick={onClick}>
+			<div className={classes.root} onClick={onClick}>
 				<FormattedMessage id='chat.inbox.scrollDownToSee' />
 			</div>
 		</Slide>
