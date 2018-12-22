824e4cb2dba030deb9f53e9eaeb31f8ce8eda687
thermarthae <thermarthae@protonmail.com>
2018-12-22 17:25:37 +0100

    use JSS in Aside

---
+++ src/components/Chat/Mailbox/Aside/Aside.style.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/styles';
+import { Theme } from '@material-ui/core';
+
+const asideStyles = makeStyles(({ palette }: Theme) => ({
+	root: {
+		marginRight: -300,
+		flex: '0 0 300px',
+		backgroundColor: palette.primary.dark,
+		transition: 'margin-right 750ms cubic-bezier(0.23, 1, 0.32, 1)',
+		willChange: 'margin-right',
+	},
+	active: {
+		marginRight: 0,
+	}
+}), { name: 'Aside' });
+export default asideStyles;
--- src/components/Chat/Mailbox/Aside/Aside.tsx
+++ src/components/Chat/Mailbox/Aside/Aside.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 
 import Query from 'react-apollo/Query';
+import asideStyles from './Aside.style';
+
 import { GET_ASIDE_STATUS } from './Aside.apollo';
 
-const Aside: React.SFC<{}> = () => {
+const Aside = () => {
+	const classes = asideStyles({});
 	return (
 		<Query query={GET_ASIDE_STATUS}>
 			{({ data: { chat: { isAsideOpen } } }) =>
-				<div className={'aside' + (isAsideOpen ? ' active' : '')}></div>
+				<div className={classes.root + (isAsideOpen ? (' ' + classes.active) : '')}></div>
 			}
 		</Query>
 	);
