30dd5f8b97289cb34c7375dbd0f026b6f04e175d
thermarthae <thermarthae@protonmail.com>
2019-01-18 18:57:57 +0100

    OptionList: Fix palette arg

--- src/components/OptionList/OptionList.tsx
+++ src/components/OptionList/OptionList.tsx
@@ -3,7 +3,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 import { styled } from '@material-ui/styles';
 
-const OptionList = styled(MenuItem)(({ palette }: Theme) => ({
+const OptionList = styled(MenuItem)(({ theme: { palette } }: { theme: Theme }) => ({
 	color: palette.text.primary,
 	height: 'auto',
 	fontSize: 'inherit',
