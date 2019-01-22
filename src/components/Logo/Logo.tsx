f7dc65d51c4c5b22ea30f4d8e0fd3713802c7e40
thermarthae <thermarthae@protonmail.com>
2019-01-22 19:58:55 +0100

    Refactor Logo default size

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,19 +5,20 @@ import logoStyles from './Logo.style';
 import logo from '@src/public/logo.png';
 
 interface ILogo {
-	size: number;
+	size?: number;
 	className?: string;
 }
 
-const Logo = React.memo(({ size, className }: ILogo = { size: 4 }) => {
+const Logo = React.memo((props: ILogo) => {
 	const classes = logoStyles();
+	const size = props.size || 4;
 
 	const width = 21 * size;
 	const height = 7 * size;
 
 	return (
 		<img
-			className={classNames(classes.root, className)}
+			className={classNames(classes.root, props.className)}
 			src={logo}
 			alt='Logo'
 			width={width}
