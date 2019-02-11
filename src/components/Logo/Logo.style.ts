2a05af220a55530a476c0bdf8922f9de5d18863e
thermarthae <thermarthae@protonmail.com>
2019-02-11 17:24:49 +0100

    Fix Logo shrinking

--- src/components/Logo/Logo.style.ts
+++ src/components/Logo/Logo.style.ts
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/styles';
 
 const logoStyles = makeStyles({
 	root: {
+		flexShrink: 0,
 		imageRendering: 'pixelated',
 		fallbacks: { imageRendering: 'crisp-edges' }
 	}
