98c712c8cd5e00b386942555562b158edb6e2708
thermarthae <thermarthae@protonmail.com>
2019-03-04 17:27:29 +0100

    Refactor Chat to function component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,39 +1,28 @@
 import React from 'react';
+import { useMutation } from 'react-apollo-hooks';
 import { RouteComponentProps } from 'react-router-dom';
-import { withStyles } from '@material-ui/styles';
-
-import chatStyles, { TChatStyles } from './Chat.style';
 
 import Conversations from './Conversations/Conversations';
 import Mailbox from './Mailbox/Mailbox';
-import { Mutation } from 'react-apollo';
+
 import { SET_OPONENT_ID } from './Chat.apollo';
+import chatStyles from './Chat.style';
 
-interface IChatProps extends TChatStyles, RouteComponentProps<{ oponentId?: string }> { }
+interface IChatProps extends RouteComponentProps<{ oponentId?: string }> { }
 
-class Chat extends React.Component<IChatProps> {
-	public shouldComponentUpdate(nextProps: IChatProps) {
-		if (JSON.stringify(this.props.match) !== JSON.stringify(nextProps.match)) return true;
-		return false;
-	}
+const Chat = ({ match }: IChatProps) => {
+	const oponentId = match.params.oponentId;
+	const classes = chatStyles();
 
-	public render() {
-		const oponentId = this.props.match.params.oponentId;
-		const { classes } = this.props;
+	const setOponentId = useMutation(SET_OPONENT_ID, { variables: { id: oponentId } });
+	setOponentId();
 
-		return (
-			<Mutation mutation={SET_OPONENT_ID} ignoreResults>
-				{setOponentId => {
-					setOponentId({ variables: { id: oponentId } });
-					return (
-						<div className={classes.root}>
-							<Conversations />
-							<Mailbox oponentId={oponentId} />
-						</div>
-					);
-				}}
-			</Mutation>
-		);
-	}
-}
-export default withStyles(chatStyles)(Chat);
+	return (
+		<div className={classes.root}>
+			<Conversations />
+			<Mailbox oponentId={oponentId} />
+		</div>
+	);
+};
+
+export default Chat;
