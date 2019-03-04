7aad230d12558e8dbdab2571361f247fd2056367
thermarthae <thermarthae@protonmail.com>
2019-03-04 16:43:08 +0100

    Use apollo hooks in Header

--- src/components/Chat/Mailbox/Header/Header.tsx
+++ src/components/Chat/Mailbox/Header/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
-import Mutation from 'react-apollo/Mutation';
-import { TOGGLE_ASIDE } from './Header.apollo';
+import { useMutation } from 'react-apollo-hooks';
 
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import SettingsSharp from '@material-ui/icons/SettingsSharp';
 
+import { TOGGLE_ASIDE } from './Header.apollo';
 import headerStyles from './Header.style';
 
 interface IHeaderProps {
@@ -15,21 +14,20 @@ interface IHeaderProps {
 
 const Header = React.memo(({ conversationName }: IHeaderProps) => {
 	const classes = headerStyles();
+	const toggleAsideMutation = useMutation(TOGGLE_ASIDE);
+	const toggleAside = () => toggleAsideMutation();
+
 	return (
-		<Mutation mutation={TOGGLE_ASIDE}>
-			{toggleAside =>
-				<div className={classes.root}>
-					<Typography className={classes.id} variant='subtitle2'>{conversationName}</Typography>
-					<ButtonBase
-						focusRipple
-						className={classes.btn}
-						onClick={() => toggleAside()}
-					>
-						<SettingsSharp className={classes.ico} />
-					</ButtonBase>
-				</div>
-			}
-		</Mutation>
+		<div className={classes.root}>
+			<Typography className={classes.id} variant='subtitle2'>{conversationName}</Typography>
+			<ButtonBase
+				focusRipple
+				className={classes.btn}
+				onClick={toggleAside}
+			>
+				<SettingsSharp className={classes.ico} />
+			</ButtonBase>
+		</div>
 	);
 });
 
