fe0ba5b6fc469b5001bd6b459bfaacb167be517c
thermarthae <thermarthae@protonmail.com>
2019-03-23 20:01:11 +0100

    Remove Subscriptions from local state

--- src/state/index.ts
+++ src/state/index.ts
@@ -2,11 +2,13 @@ import deepmerge from 'deepmerge';
 
 import app from './App.state';
 import chat from './Chat.state';
-import subscriptions from './Subscriptions.state';
 
 interface IClientState {
 	defaults: object;
 	resolvers: object;
 }
 
-export default deepmerge.all<IClientState>([app, chat, subscriptions]);
+export default deepmerge.all<IClientState>([
+	app,
+	chat
+]);
