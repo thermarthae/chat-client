ab416d257a2825d8f66012214fb054af970a4b09
thermarthae <thermarthae@protonmail.com>
2019-03-27 19:25:54 +0100

    Revert "Remove react-apollo ApolloProvider"
    This reverts commit bb2264f4423d569d4fe5da4ed520dca2c6de01b9.
    Login component is still using react-apollo.
    Undo it after Login component refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import './bootstrap';
 import React from 'react';
 import { render } from 'react-dom';
 import { hot } from 'react-hot-loader/root';
-import { ApolloProvider } from 'react-apollo-hooks';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { ThemeProvider } from '@material-ui/styles';
 
 import { client } from './providers/apolloClient';
@@ -13,9 +14,11 @@ import App from './components/App';
 
 const WrappedApp = () => (
 	<ApolloProvider client={client}>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
+		<ApolloHooksProvider client={client}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</ApolloHooksProvider>
 	</ApolloProvider>
 );
 
