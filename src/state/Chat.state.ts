52aac2228866a2b10cccc491e33c452895cc82f5
thermarthae <thermarthae@protonmail.com>
2019-03-29 18:58:37 +0100

    Remove isAsideOpen from apollo state

--- src/state/Chat.state.ts
+++ src/state/Chat.state.ts
@@ -1,9 +1,7 @@
-import gql from 'graphql-tag';
 import { IClientState } from '.';
 
 interface IChat {
 	__typename: 'Chat';
-	isAsideOpen: boolean;
 	oponentId: string | null;
 }
 
@@ -11,32 +9,11 @@ const Chat: IClientState<'chat', IChat> = {
 	defaults: {
 		chat: {
 			__typename: 'Chat',
-			isAsideOpen: false,
 			oponentId: null,
 		}
 	},
 	resolvers: {
 		Mutation: {
-			toggleAside: ({ }, { }, { cache }) => {
-				const query = gql`
-					query getAsideStatus {
-						chat @client {
-							isAsideOpen
-						}
-					}
-				`;
-				const { chat: { isAsideOpen } } = cache.readQuery<Record<'chat', IChat>>({ query })!;
-
-				const data = {
-					chat: {
-						__typename: 'Chat',
-						isAsideOpen: !isAsideOpen
-					}
-				};
-
-				cache.writeData({ data });
-				return data.chat;
-			},
 			setOponentId: ({ }, { oponentId }, { cache }) => {
 				const data = {
 					chat: {
