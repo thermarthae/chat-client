af40d858b52bb62ef29928368ce3c3d8186c28fc
thermarthae <thermarthae@protonmail.com>
2019-04-20 21:10:59 +0200

    Add Jewels component

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -4,15 +4,10 @@ import { useApolloClient } from 'react-apollo-hooks';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import PowerSettingsNew from '@material-ui/icons/PowerSettingsNew';
 
 import navigatorStyles from './Navigator.style';
-import LinkIconButton from '../LinkButtons/LinkIconButton';
 import Logo from '../Logo/Logo';
+import Jewels from './Jewels/Jewels';
 
 import { LOGOUT } from './Navigator.apollo';
 import LoginStatusCtx from '@src/context/LoginStatus';
@@ -38,19 +33,7 @@ const MenuAppBar = React.memo<INavigatorProps>(() => {
 			<Toolbar className={classes.toolbar}>
 				<Logo />
 				<Typography variant='h6' color='inherit' className={classes.grow} />
-				<div>
-					<LinkIconButton exact={false} to='/chat'>
-						<Badge color='error' badgeContent={'?'} >
-							<MailIcon titleAccess='Chat' />
-						</Badge>
-					</LinkIconButton>
-					<LinkIconButton to='/login'>
-						<AccountCircle titleAccess='Login' />
-					</LinkIconButton>
-					<IconButton color='inherit' onClick={handleLogout}>
-						<PowerSettingsNew titleAccess='Logout' />
-					</IconButton>
-				</div>
+				<Jewels onLogout={handleLogout} />
 			</Toolbar>
 		</AppBar>
 	);
