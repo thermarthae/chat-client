d7129cd0dbd83be83064ad6ccafe1ebe45b17ce1
thermarthae <thermarthae@protonmail.com>
2019-05-24 20:27:40 +0200

    Add _id to getUserConversations query
    This will provide better apollo caching

--- src/components/Navigator/Jewels/Jewels.apollo.ts
+++ src/components/Navigator/Jewels/Jewels.apollo.ts
@@ -3,12 +3,14 @@ import gql from 'graphql-tag';
 export const GET_CONV_SEEN = gql`
 	query getConvSeen {
 		getUserConversations {
+			_id
 			seen
 		}
 	}
 `;
 export interface IGetConvSeenRes {
 	getUserConversations: Array<{
+		_id: string;
 		seen: string;
 	}>;
 }
